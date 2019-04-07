864df5cf0e961882075e49c4d2e7bd699475c822
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-07 12:51:16 -0600

    Type conversion part 1

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-// Primitive
+let val;
 
-// String
-const name = 'John Doe';
-// Number
-const age = 45;
-// Boolean
-const hasKids = true;
-// Null
-const car = null;
-// Undefined
-let test;
-// Symbol
-const sym = Symbol();
+// Number to string
+val = String(555);
+val = String(4+4);
+// Boolean to string
+val = String(true);
+// Date to string
+val = String(new Date());
+// Array to string
+val = String([1,2,3,4]);
 
-// Reference Types - Objects
-// Array
-const hobbies = ['movies', 'music'];
-// Object literal 
-const address = {
-    city: 'Boston',
-    state: 'MA'
-}
-const today = new Date();
-console.log(today);
-console.log(typeof today);
+// toString()
+val = (5).toString();
+val = (true).toString();
+
+// String to number
+val = Number('5');
+val = Number(true);
+
+// Output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed());
