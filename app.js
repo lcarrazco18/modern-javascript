df7bf7bc0425e48ddc3a893a56b69535e748cbe6
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-07 11:43:21 -0600

    Data types / Primitive types

--- app.js
+++ app.js
@@ -1,50 +1,26 @@
-// var, let, const
+// Primitive
 
-// var name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
 
-// Init var
-// var greeting;
-// console.log(greeting);
-// greeting = 'Hello';
-// console.log(greeting);
-
-// letters, numbers, _, $
-// Can not start with number
-
-// Multi word vars
-// var firstName = 'John'; // Camel case
-// var first_name = 'Sara'; // Underscore
-// var FirstName = 'Tom'; // Pascal case
-// var firstName;
-
-// Let
-// let name;
-// name = 'John Doe';
-// console.log(name);
-// name = 'Steve Smith';
-// console.log(name);
-
-// Const
-// const name = 'John';
-// console.log(name);
-// Can not reassign
-// name = 'Sara';
-// Have to assign a value
-// const greeting;
-
-// const person = {
-//     name: 'John',
-//     age: 30
-// }
-
-// person.name = 'Sara';
-// person.age = 32;
-
-// // console.log(person);
-// const numbers = [1,2,3,4,5];
-// numbers.push(6);
-
-// console.log(numbers);
+// Reference Types - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal 
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+console.log(today);
+console.log(typeof today);
