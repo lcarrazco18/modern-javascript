d6803b2ed717d3a9c16176f351ef4242f7244ad1
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-08 19:54:45 -0600

    Type Conversion part 2

--- app.js
+++ app.js
@@ -17,9 +17,23 @@ val = (true).toString();
 // String to number
 val = Number('5');
 val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1,2,3]);
+
+val = parseInt('100.30');
+val = parseFloat('100.30')
 
 // Output
-console.log(val);
-console.log(typeof val);
-// console.log(val.length);
-console.log(val.toFixed());
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = 6;
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
