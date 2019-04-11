baa5d211a254d6ddfa546441b3d3d4952b239814
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-11 17:18:26 -0600

    Object literals

--- app.js
+++ app.js
@@ -1,61 +1,47 @@
-// Create some arrays
-
-const numbers = [43,56,33,23,44,36,5];
-const numbers2 = new Array(22,45,33,76,54);
-const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
-const mixed = [22, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+const person = {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    age: 38,
+    email: 'steve@aol.com',
+    hobbies: ['music', 'sports'],
+    address: {
+        city: 'Miami',
+        state: 'FL'
+    },
+    getBirthYear: function () {
+        return 2019 - this.age;
+    }
+}
 
 let val;
 
-// Get array length
-val = numbers.length;
-// Check if is array
-val = Array.isArray(numbers);
+val = person;
+// Get specific value
+val = person.firstName;
+val = person['firstName'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
 
-// Get single value
-val = numbers[3];
-val = numbers[0];
-// Insert into array
-numbers[2] = 100;
-// Find index of value;
-val = numbers.indexOf(36);
 
-// // Mutating Arrays
-// // Add on to end
-// numbers.push(250);
-// // Add on to front
-// numbers.unshift(120);
-// // Take off from end
-// numbers.pop();
-// // Take off from the front
-// numbers.shift();
-// // Splice values
-// numbers.splice(1,3);
-// // Reverse
-// numbers.reverse();
-
-// Concatenate array
-val = numbers.concat(numbers2);
-
-// // Sorting arrays
-// val = fruit.sort();
-// val = numbers.sort();
-
-// // Use the "compare function"
-// val = numbers.sort(function(x, y){
-//     return x - y;
-// });
-// // Reverse sort
-// val = numbers.sort(function(x, y){
-//     return y - x;
-// });
-
-// Find
-function over50(num){
-return num > 50;
-}
-
-val = numbers.find(over50);
-
-console.log(numbers);
 console.log(val);
+
+const people = [{
+        name: 'John',
+        age: 30
+    },
+    {
+        name: 'Mike',
+        age: 23
+    },
+    {
+        name: 'Nancy',
+        age: 36
+    }
+];
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].name);
+}
