8962780eaddd6772dff40c293f93b53490cf6b31
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-13 13:50:45 -0600

    Function Declerations

--- app.js
+++ app.js
@@ -1,71 +1,48 @@
 /* eslint-disable no-console */
-// const color = 'red';
 
-// switch(color){
-//     case 'red':
-//     console.log('Color is red');
-//     break;
-//     case 'blue':
-//     console.log('Color is blue');
-//     break;
-//     default:
-//     console.log('Color is not red or blue');
-// }
-
-// let day;
-
-// switch(new Date().getDay()){
-// case 0:
-//     day = 'Sunday';
-//     break;
-// case 1:
-//     day = 'Monday';
-//     break;
-// case 2:
-//     day = 'Tuesday';
-//     break;
-// case 3:
-//     day = 'Wednesday';
-//     break;
-// case 4:
-//     day = 'Thursday';
-//     break;
-// case 5:
-//     day = 'Friday';
-//     break;
-// case 6:
-//     day = 'Saturday';
-//     break;
-// }
-
-// console.log(`Today is ${day}`);
-
-let day = new Date().getDay();
-console.log(day)
-switch (day) {
-  case 0:
-    day = "Sunday"
-    break;
-  case 1:
-    day = "Monday"
-    break;
-  case 2:
-    day = "Tuesday"
-    break;
-  case 3:
-    day = "Wednesday"
-    break;
-  case 4:
-    day = "Thursday"
-    break;
-  case 5:
-    day = "Friday"
-    break;
-  case 6:
-    day = "Saturday"
-    break;
-  default:
-    day = "blueberry"
+function greet(firstName = 'John', lastName = 'Doe'){
+//   if(typeof firstName === 'undefined'){firstName = 'John'}
+//   if(typeof lastName === 'undefined'){lastName = 'Doe'}
+  // console.log('Hello');
+  return 'Hello ' + firstName + ' ' + lastName;
 }
 
-console.log(`Today is ${day}`, day, new Date().getDay());
+console.log(greet());
+
+// Function Expresions
+
+const square = function(x = 3){
+  return x*x;
+};
+
+// console.log(square());
+
+
+// IIFEs Immidiately Invoked Function Expressions
+
+// (function(){
+//   console.log('IIFE Ran..')
+// })();
+
+// (function(name){
+//   console.log('Hello ' + name);
+// })('Luis');
+
+// Property Methods
+
+const todo = {
+  add: function(){
+    console.log('Add todo..')
+  },
+  edit: function(id){
+    console.log(`Edit todo ${id}`);
+  }
+}
+
+todo.delete = function(){
+  console.log('Delete todo...');
+}
+
+todo.add();
+todo.edit(22);
+todo.delete();
