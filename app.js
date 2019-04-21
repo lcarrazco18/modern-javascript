4cb9683b0bc2e4a4e03a846efb924d700c890cb3
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-20 21:56:14 -0600

    The document object part 1

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
+let val;
 
-// Global scope
-var a = 1;
-let b = 2;
-const c = 3;
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.contentType;
 
-// function test() {
-//     var a = 4;
-//     let b = 5;
-//     const c = 6;
-//     console.log('Function Scope: ', a, b, c);
-// }
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
 
-// test();
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+val = document.links[0].classList[0];
 
-// if(true) {
-//     // Block Scope
-//     var a = 4;
-//     let b = 5;
-//     const c = 6;
-//     console.log('If Scope: ', a, b, c);
-// }
-
-for(let a = 0; a < 10; a++) {
-    
-    console.log(`Loop: ${a}`);
-}
-
-console.log('Global Scope: ', a, b, c);
+console.log(val);
