57bae755cd9ad76cbe2b3006023c5a167bece4e4
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-24 20:14:43 -0600

    Traversing the DOM part 2

--- app.js
+++ app.js
@@ -78,6 +78,35 @@ val = list.childNodes[3].nodeType;
 // Get children element nodes
 val = list.children;
 val = list.children[1];
+list.children[1].textContent = 'Hello';
+// Children of children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children[0];
+
+val = list.firstChild;
+val = list.firstElementChild;
+
+// last child
+
+val = list.lastChild;
+val = list.lastElementChild;
+
+
+// Count child elements
+val = list.childElementCount;
+
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
+
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
+
+// Get previous sibling
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling;
 
 console.log(val);
 
