48a7c26980b121bacbbba8b2aa401cfae2026479
Luis Carrazco <luiscarrazco@gmail.com>
2019-04-26 16:10:58 -0600

    creating an element

--- app.js
+++ app.js
@@ -1,113 +1,29 @@
-// document.getelementsByClassName
+// Create element
+const li = document.createElement('li');
 
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
+// Add class
+li.className = 'collection-item';
 
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// Add id
+li.id = 'new-item';
 
-// console.log(listItems);
+// Add attribute
+li.setAttribute('title', 'New Item');
 
-// document.getElementsByTagName
-// let lis = document.getElementsByTagName('li');
-// console.log(lis);
-// console.log(lis[0]);
-// lis[0].style.color = 'red';
-// lis[3].textContent = 'Hello';
+// Create text node and append
+li.appendChild(document.createTextNode('Hello World'));
 
-// // Conver HTML collection into array
-// lis = Array.from(lis);
+// Create new link element
+const link = document.createElement('a');
+// Add class
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHtml = '<i class="fas fa-times"></i>';
 
-// lis.reverse();
+// Append link into li
+li.appendChild(link);
 
-// lis.forEach(function(li, index){
-//     console.log(li.className);
-//     li.textContent = `${index}: Hello`;
-// });
+// Append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
 
-// console.log(lis);
-
-// document.querySelectorAll()
-// const items = document.querySelectorAll('ul.collection li.collection-item');
-
-// items.forEach(function (item, index) {
-//     item.textContent = `${index}: Hello`;
-// });
-
-// const liOdd = document.querySelectorAll('li:nth-child(odd)');
-// const liEven = document.querySelectorAll('li:nth-child(even)');
-
-// liOdd.forEach(function (li, index) {
-//     li.style.background = '#ccc';
-// });
-
-// for(let i = 0; i < liEven.length; i++){
-//   liEven[i].style.background = '#f4f4f4';
-// };
-
-// console.log(items);
-
-////////////////////////////////////////////////////////////////////////
-// Traversing the DOM
-
-let val;
-
-const list = document.querySelector('ul.collection');
-const listItem = document.querySelector('li.collection-item:first-child');
-
-val = listItem;
-val = list;
-
-// Get child nodes
-// val = list.childNodes;
-// val = list.childNodes[0];
-// val = list.childNodes[0].nodeName;
-// val = list.childNodes[3].nodeType;
-
-
-// 1 - Element
-// 2 - Attribute (deprecated)
-// 3 - Text node
-// 8 - Comment
-// 9 - Document itself
-// 10 - Doctype
-
-
-// Get children element nodes
-// val = list.children;
-// val = list.children[1];
-// list.children[1].textContent = 'Hello';
-// // Children of children
-// list.children[3].children[0].id = 'test-link';
-// val = list.children[3].children[0];
-
-// val = list.firstChild;
-// val = list.firstElementChild;
-
-// last child
-
-// val = list.lastChild;
-// val = list.lastElementChild;
-
-
-// Count child elements
-// val = list.childElementCount;
-
-// Get parent node
-// val = listItem.parentNode;
-// val = listItem.parentElement;
-// val = listItem.parentElement.parentElement;
-
-// Get next sibling
-// val = listItem.nextSibling;
-// val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
-
-// Get previous sibling
-// val = listItem.previousSibling;
-// val = listItem.previousElementSibling;
-
-// console.log(val);
-
-
+console.log(li);
