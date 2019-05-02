ddacb07de3c3ed75027b073b4fda24c3e6de93bd
Luis Carrazco <luiscarrazco@gmail.com>
2019-05-02 15:04:03 -0600

    classes and attributes part 1

--- app.js
+++ app.js
@@ -3,26 +3,41 @@
 // Create Element
 const newHeading = document.createElement('h2');
 // Add id
-newHeading.id = 
-'task-title';
+newHeading.id = 'task-title';
 // New text node
 newHeading.appendChild(document.createTextNode('Task List'));
 
 // Get the old heading
 const oldHeading = document.getElementById('task-title');
 // Parent
-const cardAdction = document.querySelector('.card-action'
-);
-//Replace
-cardAdction.replaceChild(newHeading, oldHeading)
-;
-// Remove Element
+const cardAdction = document.querySelector('.card-action');
 
+//Replace
+cardAdction.replaceChild(newHeading, oldHeading);
+
+// Remove Element
 const lis = document.querySelectorAll('li');
 const list = document.querySelector('ul');
-// Remove list item
+
+//Remove list item
 lis[0].remove();
 
-// Remove child element
-list.removeChild(list[3]);
+//Remove child element
+list.removeChild(lis[3]);
+
+// Classes & ATTR
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
+
+let val;
+
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+console.log(val);
+
 
