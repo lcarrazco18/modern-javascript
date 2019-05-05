835535894bbc1620219877fee9068cb9a4e0311a
Luis Carrazco <luiscarrazco@gmail.com>
2019-05-05 12:03:03 -0600

    Event Listener

--- app.js
+++ app.js
@@ -1,52 +1,5 @@
-// Replace Element
-
-// Create Element
-const newHeading = document.createElement('h2');
-// Add id
-newHeading.id = 'task-title';
-// New text node
-newHeading.appendChild(document.createTextNode('Task List'));
-
-// Get the old heading
-const oldHeading = document.getElementById('task-title');
-// Parent
-const cardAdction = document.querySelector('.card-action');
-
-//Replace
-cardAdction.replaceChild(newHeading, oldHeading);
-
-// Remove Element
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
-
-//Remove list item
-lis[0].remove();
-
-//Remove child element
-list.removeChild(lis[3]);
-
-// Classes & ATTR
-const firstLi = document.querySelector('li:first-child');
-const link = firstLi.children[0];
-
-let val;
-
-// Classes
-val = link.className;
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test');
-link.classList.remove('test');
-val = link;
-
-// Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href', 'http://www.google.com');
-link.setAttribute('title', 'google');
-val = link.hasAttribute('title');
-link.removeAttribute('title');
-val = link;
-
-console.log(val);
-
+// Event Listeners
+document.querySelector('.clear-tasks').addEventListener('click', function(){
+    console.log('hello world');
+});
 
