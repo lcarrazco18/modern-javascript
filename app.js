86cea80e3b1ceee01ec5bddbf5baf15bf5e96364
Luis Carrazco <luiscarrazco@gmail.com>
2019-05-09 17:28:43 -0600

    local & session storage

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-// // Event Bubbling
+// // Set local storage item
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', '38');
 
-// document.querySelector('.card-title').addEventListener('click', function(){
-//   console.log('card title');
-// });
+// // Set session storage item
+// sessionStorage.setItem('name', 'Beth');
 
-// document.querySelector('.card-content').addEventListener('click', function(){
-//   console.log('card content');
-// });
+// remove from storage
+// localstorage.removeItem('name');
 
-// document.querySelector('.card').addEventListener('click', function(){
-//   console.log('card');
-// });
+// get from storage
 
-// document.querySelector('.col').addEventListener('click', function(){
-//   console.log('col');
-// });
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
 
-// Event Delegation
+// clear local storage
+// localStorage.clear();
+// console.log(name, age);
 
-// const delItem = document.querySelector('.delete-item');
+document.querySelector('form').addEventListener('submit', function(e){
+  const task = document.getElementById('task').value;
 
-// delItem.addEventListener('click', deleteItem);
+  let tasks;
 
-
-document.body.addEventListener('click', deleteItem);
-
-function deleteItem(e){
-//   if(e.target.parentElement.className === 'delete-item secondary-content'){
-//     console.log('delete item');
-//   }
-// }
-
-
-  if(e.target.parentElement.classList.contains('delete-item')){
-    console.log('delete item');
-    e.target.parentElement.parentElement.remove();
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
   }
-}
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  alert('Task Saved');
+
+  e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task){
+  console.log(task);
+});
